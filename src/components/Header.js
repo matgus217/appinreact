5f8af4cd17abf27c9230c8a633db7b0a765b2c4c
matgus217 <matgus217@gmail.com>
2024-03-27 11:09:35 +0000

    Change color infooter

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ function Header() {
     return (
         <header>
             <h1 className="header-title">Get It Done!</h1>
+            <p className="header-p">Add your day. Get everything done.</p>
         </header>
     )
 }
